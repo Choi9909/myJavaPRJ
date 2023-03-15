cb9e27dc505e63e4615dfe67ac41cf164ffc77d9
choi9909 <choi16289@naver.com>
2023-03-15 09:35:21 +0900

    plz Yes24

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args)
     {
         System.out.println("Hello!! My First Java Coding!!");
+        System.out.println("yes24 왜 책 안보내줘 빨리 보내줘")
     }
 }
