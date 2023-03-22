22013bfe18a7e1a777cdd1da3ab56bb4128acd2c
choi9909 <choi16289@naver.com>
2023-03-22 09:28:07 +0900

    .

---
+++ src/main/java/Ex_2/Ex02_01.java
@@ -0,0 +1,24 @@
+package Ex_2;
+
+public class Ex02_01 {
+    public static void main(String[]args){
+        //TO DO Auto-generated method stub
+        int a, b;
+        int result;
+
+        a = 100;
+        b = 50;
+
+        result = a + b;
+        System.out.println(a + "+"+ b +"="+ result);
+
+        result = a - b;
+        System.out.println(a +"-"+ b + "="+ result);
+
+        result = a * b;
+        System.out.println(a+"*"+b+"="+result);
+
+        result = a/b;
+        System.out.println(a+"/"+b+"="+result);
+    }
+}
---
+++ src/main/java/Ex_3/Ex03_01.java
@@ -0,0 +1,10 @@
+package Ex_3;
+
+public class Ex03_01 {
+    public static void main(String[] args) {
+        System.out.printf("100+100");
+        System.out.printf("\n");
+        System.out.printf("%d", 100 + 100);
+        System.out.printf("\n");
+    }
+}
