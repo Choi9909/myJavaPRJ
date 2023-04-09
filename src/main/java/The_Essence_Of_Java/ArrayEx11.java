de2c54ef61d928ce06dc5875ca4df527106478ec
Choi9909 <choi16289@naver.com>
2023-04-09 14:01:37 +0900

    23.04.09 The Essence of Java ArrayEx

---
+++ src/main/java/The_Essence_Of_Java/ArrayEx11.java
@@ -0,0 +1,20 @@
+package The_Essence_Of_Java;
+
+public class ArrayEx11 {
+    public static void main(String[] args) {
+        int[] numArr = new int[10];
+        int[] counter = new int[10];
+
+        for (int i=0; i < numArr.length ; i++){
+            numArr[i] = (int)(Math.random() * 10);
+            System.out.print(numArr[i]);
+        }//end of for1
+        System.out.println();
+        for(int i=0; i < numArr.length; i++){
+            counter[numArr[i]]++; // numArr[i]의 값이 그 숫자의 couter[]을 하나 키워서 숫자의 개수를 알수있다.
+        }// end of for2
+        for(int i=0; i < numArr.length; i++){
+            System.out.println(i +"의 개수 : " + counter[i]  );
+        }// end of for3
+    }//end of main
+}// end of class
