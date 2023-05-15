67e6b64746aa6688d7db9916eafe7315f03f884c
Choi9909 <choi16289@naver.com>
2023-05-15 17:17:33 +0900

    자바의 정석 매서드 호출, return문 공부

---
+++ src/main/java/The_Essence_Of_Java/ch6/MyMathTest.java
@@ -0,0 +1,17 @@
+package The_Essence_Of_Java.ch6;
+
+public class MyMathTest {
+    public static void main(String[] args) {
+        MyMath mm = new MyMath();
+        long result1 = mm.add(5L, 3L);
+        long result2 = mm.substract(5L, 3L);
+        long result3 = mm.multiply(5L, 3L);
+        double result4 = mm.divid(5L, 3L);
+
+        System.out.println("add(5L, 3L) = " + result1);
+        System.out.println("subtract(5L, 3L) = " + result2);
+        System.out.println("multiply(5L, 3L) = " + result3);
+        System.out.println("divide(5L, 3L) = " + result4);
+    }
+}
+ 
